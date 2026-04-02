ae5b5c77f3b25ab742c7cb689306be5520bd5b8e
Sai Shirfule <saishirfule9@gmail.com>
2026-04-02 10:43:05 +0530

    Create RoleToggle.js

---
+++ src/data/components/RoleToggle.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function RoleToggle({ role, setRole }) {
+  return (
+    <select value={role} onChange={(e) => setRole(e.target.value)}>
+      <option value="viewer">Viewer</option>
+      <option value="admin">Admin</option>
+    </select>
+  );
+}
