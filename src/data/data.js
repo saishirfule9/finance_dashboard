20bde1412a920ea72f4102e81eeb8eca19e97da9
Sai Shirfule <saishirfule9@gmail.com>
2026-04-02 10:42:38 +0530

    Create data.js

---
+++ src/data/data.js
@@ -0,0 +1,7 @@
+const data = [
+  { id: 1, date: "2026-04-01", amount: 5000, category: "Salary", type: "income" },
+  { id: 2, date: "2026-04-02", amount: 1200, category: "Food", type: "expense" },
+  { id: 3, date: "2026-04-03", amount: 2000, category: "Shopping", type: "expense" }
+];
+
+export default data;
